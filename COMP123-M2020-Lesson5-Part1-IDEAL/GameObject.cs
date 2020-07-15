0641a2c1c24d87e0d9b578e71a9bc3f249f116e3
Hussam Eldin Mohamed <dr.hussam@live.com>
2020-07-14 20:30:49 -0400

    Added GameObject class

---
+++ COMP123-M2020-Lesson5-Part1-IDEAL/GameObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_Lesson5_Part1_IDEAL
+{
+    class GameObject
+    {
+        // PRIVATE INSTANCE MEMBERS 
+        private string m_name;
+
+        // PUBLIC PROPERTIES
+        public Transform Transform { get; set; }
+
+        public string Name 
+        {
+            get
+            {
+                return m_name;
+            }
+            set
+            {
+                m_name = value;
+            }
+        }
+
+        // CONSTRUCTOR(S)
+        public GameObject(string name)
+        {
+            Name = name;
+        }
+
+        // PRIVATE METHODS 
+
+
+        // PUBLIC METHODS 
+
+    }
+}
---
+++ COMP123-M2020-Lesson5-Part1-IDEAL/Transform.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_Lesson5_Part1_IDEAL
+{
+    struct Transform
+    {
+        public Vector2D postion;
+        public Vector2D rotation;
+        public Vector2D scale;
+    }
+}
